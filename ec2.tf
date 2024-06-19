8bafd678f7d52bdaa34f5ac805d39167ed91dee1
Gary Dahnke <garydahnke@sbcglobal.net>
2024-06-19 18:59:06 -0400

    second commit

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "ec2_virginia" {
     ami = "ami-0bb84b8ffd87024d8"
     instance_type = "t2.micro"
-    provider = aws.virginia
     tags = {
         Name = "Virginia"
     }
@@ -10,7 +9,6 @@ resource "aws_instance" "ec2_virginia" {
 resource "aws_instance" "ec2_ohio" {
     ami = "ami-033fabdd332044f06"
     instance_type = "t2.micro"
-    provider = aws.ohio
     tags = {
         Name = "Ohio"
     }
