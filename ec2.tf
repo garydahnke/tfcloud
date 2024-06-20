e0e4ae12ec83c49cad8f84ebd8cc99313a0b0ab2
Gary Dahnke <garydahnke@sbcglobal.net>
2024-06-19 22:42:23 -0400

    commit

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2_virginia" {
     ami = "ami-0bb84b8ffd87024d8"
     instance_type = "t2.micro"
-    tags = {
-        Name = "Virginia"
-    }
+#    tags = {
+#        Name = "Virginia"
+#    }
 }
