35697758757cc582bacc214a7772c99e5310bf27
Gary Dahnke <garydahnke@sbcglobal.net>
2024-09-10 15:36:19 -0400

    Uncomment Tag statement

--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ data "aws_region" "current" {}
 resource "aws_instance" "ec2" {
     ami = lookup(var.ami_map,data.aws_region.current.id)
     instance_type = var.instance_type_list[1]
-#    tags = {
-#        Name = "my_ec2"
-#    }
+    tags = {
+        Name = "my_ec2"
+    }
 }
 
 output "aws_region_current_id" {
