f3fb478212d96cbe75ad272254a3feeaa3412457
Gary Dahnke <garydahnke@sbcglobal.net>
2025-02-04 18:17:21 -0500

    uncomment tags

--- main.tf
+++ main.tf
@@ -7,9 +7,9 @@ data "aws_region" "current" {}
 resource "aws_instance" "ec2" {
     ami = lookup(var.ami_map,data.aws_region.current.id)
     instance_type = var.instance_type_list[1]
-#    tags = {
-#        Name = var.servername
-#    }
+    tags = {
+        Name = var.servername
+    }
 }
 
 output "aws_region_current_id" {
